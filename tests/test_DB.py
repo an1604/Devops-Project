897616a6cc0d96536a3b96786877fb01a341cfda
an1604 <nataf12386@cs.colman.ac.il>
2024-07-07 13:32:21 +0300

    Test-4: Test full process!

--- tests/test_DB.py
+++ tests/test_DB.py
@@ -1,13 +1,13 @@
 import pytest
 from pymongo import MongoClient
-from utilFunc import generate_test_user
+from utilFunc import generate_test_user, generate_test_post
 
 MachineIP = '127.0.0.1'
 dbName = 'test'
-collectionName = 'users'
 
 
 def test_retrieve_data_from_users():
+    collectionName = 'users'
     client = MongoClient(f'mongodb://{MachineIP}:27017/{dbName}')
     db = client[dbName]
     test_user = generate_test_user()
@@ -26,6 +26,46 @@ def test_retrieve_data_from_users():
         assert 'imgUrl' in item
         assert 'password' in item
         assert 'posts' in item
+
+
+def test_retrieve_data_from_posts():
+    usersCollectionName = 'users'
+    postsCollectionName = 'posts'
+    client = MongoClient(f'mongodb://{MachineIP}:27017/{dbName}')
+    db = client[dbName]
+
+    # Generate and insert a test user
+    test_user = generate_test_user()
+    print(f"Test user: {test_user}")
+
+    users_collection = db[usersCollectionName]
+    user_insert_result = users_collection.insert_one(test_user)
+    user_id = user_insert_result.inserted_id
+
+    # Generate and insert a test post for the user
+    test_post = generate_test_post(user_id)
+    print(f"Test post: {test_post}")
+
+    posts_collection = db[postsCollectionName]
+    posts_collection.insert_one(test_post)
+
+    # Retrieve data from the posts' collection
+    data = list(posts_collection.find({'userId': user_id}))
+
+    assert isinstance(data, list)
+    assert len(data) > 0
+
+    for item in data:
+        assert 'userId' in item
+        assert 'title' in item
+        assert 'content' in item
+        assert 'imgUrl' in item
+        assert 'date' in item
+        assert 'numComments' in item
+
+    # Cleanup the test data
+    posts_collection.delete_one({'_id': test_post['_id']})
+    users_collection.delete_one({'_id': user_id})
 
 
 """
---
+++ tests/test_scalability.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+from test_e2e import register_user, login_user, create_post, create_comment
+from utilFunc import get_fake_email, get_fake_username, get_password
+
+MachineIP = 'localhost'
+url_prefix = f'http://{MachineIP}:5173/'
+
+
+def test_end_to_end_with_different_screen_size():
+    passed = True
+    # Initialization of all params.
+    username = get_fake_username()
+    email = get_fake_email()
+    password = get_password(8)
+    driver = webdriver.Chrome()
+
+    # Set the window size to a mobile screen size
+    driver.set_window_size(375, 667)
+
+    # Perform the tests
+    register_user(driver, username, email, password, passed)
+    login_user(driver, email, password, passed)
+    create_post(driver, passed)
+    create_comment(driver, passed)
+
+    driver.quit()
+    assert passed == True
